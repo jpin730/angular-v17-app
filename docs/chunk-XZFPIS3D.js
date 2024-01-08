471de6f113f192495eb7d590dc013ad0f82f9b54
Jaime Pineda <jpin730@pm.me>
2024-01-07 22:57:16 -0600

    chore: :rocket: build app in docs for gh pages

---
+++ docs/chunk-XZFPIS3D.js
@@ -0,0 +1,50 @@
+import {
+  Da as o,
+  Ha as r,
+  R as m,
+  wa as i,
+  xa as n,
+  ya as s
+} from './chunk-3UEDLJCJ.js'
+var u = (() => {
+  let e = class e {}
+  ;(e.ɵfac = function (a) {
+    return new (a || e)()
+  }),
+    (e.ɵcmp = m({
+      type: e,
+      selectors: [['app-comments']],
+      standalone: !0,
+      features: [r],
+      decls: 5,
+      vars: 0,
+      consts: [
+        [
+          'src',
+          'https://placehold.co/4000',
+          'alt',
+          'image placeholder',
+          1,
+          'w-100'
+        ]
+      ],
+      template: function (a, p) {
+        a & 1 &&
+          (i(0, 'h2'),
+          o(1, 'Comments'),
+          n(),
+          s(2, 'img', 0),
+          i(3, 'p'),
+          o(
+            4,
+            ` Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi iusto enim, ratione, laboriosam repudiandae tempora accusantium quo recusandae quos maiores animi totam temporibus laudantium quam? Ipsa corrupti placeat minima est.
+`
+          ),
+          n())
+      },
+      encapsulation: 2
+    }))
+  let t = e
+  return t
+})()
+export { u as CommentsComponent }
